04f960cbf3c8401af844520fb2bc015b0975ad17
DiaMargareta <hiskia2345@gmail.com>
2026-07-06 21:43:36 +0800

    Rename dockerfile temporarily

--- frontend/config.php
+++ frontend/config.php
@@ -1,13 +1,17 @@
 <?php
+
 $host = getenv("MYSQLHOST");
+$port = getenv("MYSQLPORT");
 $user = getenv("MYSQLUSER");
 $password = getenv("MYSQLPASSWORD");
 $database = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
 
 $conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Koneksi database gagal: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
+
 ?>
